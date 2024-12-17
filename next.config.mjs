2d34ba46369a5c157e4c88c3f6c3ca85358afdfd
MajorTom <invaderzgame@gmail.com>
2024-12-17 23:54:39 +0700

    [ADD] next config alias

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from "path";
+
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        config.resolve.alias['@'] = path.join(__dirname);
+        return config;
+    },
+};
 
 export default nextConfig;
